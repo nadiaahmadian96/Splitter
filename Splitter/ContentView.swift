b1100094b0aab9a0f1d6413a2156b22146972d18
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2023-01-03 14:09:00 +0330

    Initial Commit

---
+++ Splitter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Splitter
+//
+//  Created by Nadia Ahmadian on 2023-01-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
